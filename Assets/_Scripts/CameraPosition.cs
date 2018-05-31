099bd8350967644c5f6df9d3abfe005a2409226b
Intelix8996 <nikolat-repin201303@mail.ru>
2018-05-31 19:37:50 +0700

    Animation fix and Networking

---
+++ Assets/_Scripts/CameraPosition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPosition : MonoBehaviour {
+
+    [SerializeField]
+    Transform Origin;
+
+    [SerializeField]
+    Vector3 Offset;
+
+    void FixedUpdate () {
+        transform.position = Origin.position + Offset;
+	}
+}
--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : NetworkBehaviour {
 
     float x, y, vx, vy;
 
@@ -12,7 +13,13 @@ public class PlayerController : MonoBehaviour {
     Camera cam;
 
     [SerializeField]
+    GameObject NeckBone;
+
+    [Header("Camera")]
+    [SerializeField]
     float Sens = 10;
+    [SerializeField]
+    float ClampAngleX = 90, ClampAngleY = 90;
 
 
     private void Start()
@@ -22,20 +29,42 @@ public class PlayerController : MonoBehaviour {
 
     private void FixedUpdate()
     {
-        x = Input.GetAxis("Horizontal");
-        y = Input.GetAxis("Vertical");
+        if (isLocalPlayer)
+        {
+            x = Input.GetAxis("Horizontal");
+            y = Input.GetAxis("Vertical");
 
-        vx += Input.GetAxis("Mouse X") * Sens;
-        vy += Input.GetAxis("Mouse Y") * Sens;
+            if ((vx + Input.GetAxis("Mouse X") * Sens < ClampAngleX) &&
+               (vx + Input.GetAxis("Mouse X") * Sens > -ClampAngleX) &&
+               (vy + Input.GetAxis("Mouse Y") * Sens < ClampAngleY) &&
+               (vy + Input.GetAxis("Mouse Y") * Sens > -ClampAngleY))
+            {
+                vx += Input.GetAxis("Mouse X") * Sens;
+                vy += Input.GetAxis("Mouse Y") * Sens;
+            }
 
-        if (Input.GetKey(KeyCode.LeftShift))
-            y += .5f;
-        if (Input.GetKey(KeyCode.LeftAlt))
-            y -= .5f;
+            if (Input.GetKey(KeyCode.LeftShift))
+                y += .5f;
+
+            am.SetFloat("X", x);
+            am.SetFloat("Y", y);
+        }
+        else
+        {
+            /*foreach (GameObject GO in ComponentsToDisable)
+            {
+                GO.SetActive(false);
+            }*/
+
+            cam.enabled = false;
+        }
+
 
         cam.transform.localEulerAngles = new Vector3(-vy, vx, 0);
+    }
 
-        am.SetFloat("X", x);
-        am.SetFloat("Y", y);
+    private void LateUpdate()
+    {
+        NeckBone.transform.localEulerAngles = cam.transform.localEulerAngles;
     }
 }
